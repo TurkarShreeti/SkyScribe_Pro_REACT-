a13be4e45fa59eb0b2505664ddf98b4e346609e2
TurkarShreeti <shreetiturkar999@gmail.com>
2024-03-08 13:49:02 +0530

    Initial commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "79ae2ce8cfcb5a1ee3ed3652c069e0da",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
